65a6b81eee6af6243509f4a6baf24b89ba049c21
Уткин Михаил Владимирович <mutkin@blogic.ru>
2024-05-21 18:15:07 +0500

    fix bug if there is a nubmers in nickname

--- tools.py
+++ tools.py
@@ -26,11 +26,13 @@ def get_nickname_and_birthday_by_regex(raw_string):
     # just in case check a string for this format: "@ivan_pumpkin 22.11"
     result = re.search(r'(?i)@\S+\s[0-9][0-9].[0-9][0-9]', raw_string)
     if result:
+        nickname = result.group(0).split(' ')[0]
+        birthday = result.group(0).split(' ')[1]
         # If everything is ok, separate nickname and birthday
-        nickname = re.search(r'(?i)@\S+', result.group(0))
-        birthday = re.search(r'[0-9][0-9].[0-9][0-9]', result.group(0))
-        if is_date_format_ok(birthday.group(0)):
-            return nickname.group(0), birthday.group(0)
+        # nickname = re.search(r'(?i)@\S+', result.group(0))
+        # birthday = re.search(r'[0-9][0-9].[0-9][0-9]', result.group(0))
+        if is_date_format_ok(birthday):
+            return nickname, birthday
         else:
             return None
     else:
